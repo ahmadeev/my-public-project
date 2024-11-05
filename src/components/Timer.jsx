44553b2d94fba0fb34419b32f0b5cb0e429a903f
danis <ahrnadeev@gmail.com>
2024-11-05 06:00:11 +0300

    Modify count down timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -6,6 +6,11 @@ function Timer({ event, date_of_event_string }) {
 
     const [time, setTime] = useState(date_of_event);
 
+    const day_words = ["день", "дня", "дней"]
+    const hours_words = ["час", "часа", "часов"]
+    const minutes_words = ["минута", "минуты", "минут"]
+    const seconds_words = ["секунда", "секунды", "секунд"]
+
     useEffect(() => {
         const offset = new Date().getTimezoneOffset() * 60;
         const interval_id = setInterval(() => {
@@ -21,6 +26,18 @@ function Timer({ event, date_of_event_string }) {
         return number;
     }
 
+    function formatDateWord(number, words) {
+        const digit = number % 10
+        if(digit === 1) {
+            return `${number} ${words[0]}`
+        } else if (2 <= digit && digit <= 4) {
+            return `${number} ${words[1]}`
+        } else if (5 <= digit && digit <= 9 || digit === 0) {
+            return `${number} ${words[2]}`
+        }
+        return "format date error"
+    }
+
     if (date_of_event === time) {
         return (
             <>
@@ -34,12 +51,19 @@ function Timer({ event, date_of_event_string }) {
     return (
         <>
             <div className={styles.timer}>
-                <h1>{event.toUpperCase()}</h1>
+                <div className={styles.gradient}>
+                    <h1>{event.toUpperCase()}</h1>
+                </div>
                 {/*<h2>{time.getMonth()} месяцев {time.getDate()} дней {time.getHours()} часов {time.getMinutes()} минут {time.getSeconds()} секунд</h2>*/}
                 {/*<h2>{time.getMonth()} m : {time.getDate()} d : {time.getHours()} h : {time.getMinutes()} min : {time.getSeconds()} s</h2>*/}
                 <h1>
-                    {formatDate(Math.floor(time / (24 * 60 * 60)))} : {formatDate(Math.floor(time % (24 * 60 * 60) / (60 * 60)))} : {formatDate(Math.floor(time % (24 * 60 * 60) % (60 * 60) / 60))} : {formatDate(Math.floor(time % (24 * 60 * 60) % (60 * 60) % 60))}
+                    {/*{formatDate(Math.floor(time / (24 * 60 * 60)))} : {formatDate(Math.floor(time % (24 * 60 * 60) / (60 * 60)))} : {formatDate(Math.floor(time % (24 * 60 * 60) % (60 * 60) / 60))} : {formatDate(Math.floor(time % (24 * 60 * 60) % (60 * 60) % 60))}*/}
+                    {formatDateWord(Math.floor(time / (24 * 60 * 60)), day_words)} : {formatDateWord(Math.floor(time % (24 * 60 * 60) / (60 * 60)), hours_words)} : {formatDateWord(Math.floor(time % (24 * 60 * 60) % (60 * 60) / 60), minutes_words)} : {formatDateWord(Math.floor(time % (24 * 60 * 60) % (60 * 60) % 60), seconds_words)}
                 </h1>
+                {/*<h1>{formatDateWord(Math.floor(time / (24 * 60 * 60)), day_words)}</h1>*/}
+                {/*<h1>{formatDateWord(Math.floor(time % (24 * 60 * 60) / (60 * 60)), hours_words)}</h1>*/}
+                {/*<h1>{formatDateWord(Math.floor(time % (24 * 60 * 60) % (60 * 60) / 60), minutes_words)}</h1>*/}
+                {/*<h1>{formatDateWord(Math.floor(time % (24 * 60 * 60) % (60 * 60) % 60), seconds_words)}</h1>*/}
             </div>
         </>
     )
